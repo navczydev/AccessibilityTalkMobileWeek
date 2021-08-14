6fd6c120fe4d1a108542a96889385f8fb75be59c
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2021-08-14 18:29:57 -0400

    Initial commit

---
+++ app/src/androidTest/java/com/example/accessibilitytalkmobileweek/MainActivityTest.kt
@@ -0,0 +1,60 @@
+package com.example.accessibilitytalkmobileweek
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.accessibility.AccessibilityChecks
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    /* @get:Rule
+     var activityRule: ActivityScenarioRule<MainActivity> =
+         ActivityScenarioRule(MainActivity::class.java)
+ */
+    @Rule
+    @JvmField
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    init {
+        AccessibilityChecks.enable().setRunChecksFromRootView(true)
+    }
+
+    @Test
+    fun testText1() {
+        onView(withId(R.id.textview_1)).check(matches(isDisplayed()))
+        onView(withId(R.id.imageview_logo_holder)).perform(click())
+    }
+
+    @Test
+    fun testGreeting() {
+        val greeting = InstrumentationRegistry.getInstrumentation()
+            .targetContext.resources.getString(R.string.greeting)
+
+        composeTestRule.onNodeWithText(greeting).assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Share your stuff").performClick()
+    }
+
+    /*   companion object {
+           @BeforeClass
+           @JvmStatic
+           fun enableAccessibilityChecks() {
+               AccessibilityChecks.enable().setRunChecksFromRootView(true)
+           }
+       }*/
+}
---
+++ app/src/main/java/com/example/accessibilitytalkmobileweek/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.example.accessibilitytalkmobileweek
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.*
+import androidx.compose.ui.unit.dp
+import com.google.android.material.composethemeadapter.MdcTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.imageview_logo_holder).setOnClickListener { // open camera
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivity(takePictureIntent)
+            } catch (e: ActivityNotFoundException) {
+                // display error state to the user
+            }
+        }
+
+        val greetingFromCompose = findViewById<ComposeView>(R.id.greeting)
+
+        greetingFromCompose.setContent {
+            MdcTheme { // or AppCompatTheme
+                Greeting(
+                    clicked = {
+
+                        true
+                    },
+                    isFavorite = true
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(
+    onClick: () -> Unit = {},
+    clicked: () -> Boolean,
+    isFavorite: Boolean = false
+) {
+    val actionLabel = stringResource(
+        if (isFavorite) R.string.unfavorite else R.string.favorite
+    )
+
+    Column(
+        Modifier
+            // .clickable(onClick = {})
+            .fillMaxWidth()
+            .semantics(mergeDescendants = false) {
+                //  customActions = listOf(CustomAccessibilityAction(actionLabel, clicked))
+//                onLongClick("long clicked") { clicked() }
+//                onClick("single click") { clicked.invoke() }
+            },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Image(imageVector = Icons.Filled.ThumbUp, contentDescription = null)
+        IconButton(
+            onClick = {
+            },
+            modifier = Modifier
+                .semantics(mergeDescendants = true) {
+                    contentDescription = "Share your stuff"
+                }
+                .width(200.dp)
+                .height(200.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Share,
+//                contentDescription = stringResource(R.string.label_share),
+                contentDescription = null,
+                // Clear any semantics properties set on this node
+                Modifier
+                    .clearAndSetSemantics { }
+                /*Modifier.semantics {
+                    onClick("Click me to share") { clicked.invoke() }
+                }*/
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.greeting),
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(R.dimen.margin_small))
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .semantics {
+                    // liveRegion = LiveRegionMode.Assertive
+                    heading()
+                }
+        )
+    }
+}
