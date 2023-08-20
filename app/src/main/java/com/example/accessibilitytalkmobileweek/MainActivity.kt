1b261b45f50cfb90eb85e0c1d8cbf2d28a15df8a
Nav Singh <8358882+navczydev@users.noreply.github.com>
2023-08-20 13:03:09 -0400

    AGP nad other versions, targetSDK, compileSDK upgrade

--- app/src/main/java/com/example/accessibilitytalkmobileweek/MainActivity.kt
+++ app/src/main/java/com/example/accessibilitytalkmobileweek/MainActivity.kt
@@ -5,23 +5,33 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.*
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.android.material.composethemeadapter.MdcTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +52,7 @@ class MainActivity : AppCompatActivity() {
         val greetingFromCompose = findViewById<ComposeView>(R.id.greeting)
 
         greetingFromCompose.setContent {
-            MdcTheme { // or AppCompatTheme
+            MaterialTheme { // or AppCompatTheme
                 Greeting(
                     clicked = {
 
@@ -55,6 +65,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
 private fun Greeting(
     onClick: () -> Unit = {},
@@ -104,7 +115,7 @@ private fun Greeting(
         Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = stringResource(R.string.greeting),
-            style = MaterialTheme.typography.h5,
+            style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = dimensionResource(R.dimen.margin_small))
